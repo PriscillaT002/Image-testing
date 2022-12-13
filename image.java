77876e3c374be233942e5ec385ca2b3440d70e66
Priscilla Trejo <112957372+PriscillaT002@users.noreply.github.com>
2022-12-13 10:22:33 -0500

    new

--- image.java
+++ image.java
@@ -1,31 +1,16 @@
-//import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
 import javax.swing.JLabel;
-import javax.swing.JFrame;
 import javax.swing.ImageIcon;
-import java.awt.Toolkit;
-import java.awt.Dimension;
 
 public class image{
 
-    public static void main(String[] args){
-        JFrame f = new JFrame(); //creates jframe f
+  public static void main(String[] args){
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); //this is your screen size
+    JLabel lbl = new JLabel(new ImageIcon(image.class.getResource("HH.png")));
+    JOptionPane.showMessageDialog(null, lbl, "ImageDialog", 
+                                 JOptionPane.PLAIN_MESSAGE, null);
 
-        f.setUndecorated(true); //removes the surrounding border
 
-        ImageIcon image = new ImageIcon("HH.jpg"); //imports the image
 
-        JLabel lbl = new JLabel(image); //puts the image into a jlabel
-
-        f.getContentPane().add(lbl); //puts label inside the jframe
-
-        f.setSize(image.getIconWidth(), image.getIconHeight()); //gets h and w of image and sets jframe to the size
-
-        int x = (screenSize.width - f.getSize().width)/2; //These two lines are the dimensions
-        int y = (screenSize.height - f.getSize().height)/2;//of the center of the screen
-
-        f.setLocation(x, y); //sets the location of the jframe
-        f.setVisible(true); //makes the jframe visible
-    }
+  }
 }
