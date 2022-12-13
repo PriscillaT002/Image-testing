e28cddc4dd129b2c3026d77b1141766905117b9c
Priscilla Trejo <112957372+PriscillaT002@users.noreply.github.com>
2022-12-13 10:11:16 -0500

    new

--- image.java
+++ image.java
@@ -1,5 +1,31 @@
-public void writeToImage() throws MalformedURLException, IOException {
+//import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 
-    Graphics g = image.getGraphics();
-    ImageIO.write(image, "png", new File("HH.jpg"));
+public class image{
+
+    public static void main(String[] args){
+        JFrame f = new JFrame(); //creates jframe f
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); //this is your screen size
+
+        f.setUndecorated(true); //removes the surrounding border
+
+        ImageIcon image = new ImageIcon("HH.jpg"); //imports the image
+
+        JLabel lbl = new JLabel(image); //puts the image into a jlabel
+
+        f.getContentPane().add(lbl); //puts label inside the jframe
+
+        f.setSize(image.getIconWidth(), image.getIconHeight()); //gets h and w of image and sets jframe to the size
+
+        int x = (screenSize.width - f.getSize().width)/2; //These two lines are the dimensions
+        int y = (screenSize.height - f.getSize().height)/2;//of the center of the screen
+
+        f.setLocation(x, y); //sets the location of the jframe
+        f.setVisible(true); //makes the jframe visible
+    }
 }
